d81304af75e97d37781795e2347fcb74a6aa6e21
Ryderxz <victorgideao01@gmail.com>
2025-08-12 11:16:15 -0300

    add lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let pessoas = [];
+
+function adicionarAmigo() {
+    const nomePessoas = document.getElementById("amigo").value.trim();
+
+    if (nomePessoas !== "") {
+        pessoas.push(nomePessoas);
+        console.log(pessoas);
+        listaDeAmigos();
+        limparCampo();
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+}
+function limparCampo() {
+    const listaPessoa = document.getElementById("amigo");
+    listaPessoa.value = "";
+    listaPessoa.focus();
+}
+
+function listaDeAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let index = 0; index < pessoas.length; index++) {
+        const li = document.createElement("li");
+        li.textContent = pessoas[index];
+        lista.appendChild(li);
+    }
+}
